d6ddff13fdcb52c492779bfd8d7eb93e4e1bd534
PrNatali <79377175544@ya.ru>
2022-09-04 12:52:55 +0300

    search maximum

---
+++ example_06/Program.cs
@@ -0,0 +1,16 @@
+﻿int A = 3;
+int B = 5;
+int C = 8;
+int D = 1;
+int E = 4;
+
+int max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+if (D > max) max = D;
+if (E > max) max = E;
+
+Console.Write("max = ");
+Console.WriteLine(max);
